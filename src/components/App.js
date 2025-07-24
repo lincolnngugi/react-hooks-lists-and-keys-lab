732ab5e34f4e08af5b7cb6bb49ac913dada19a24
lincolnngugi <lincolnngugi6@gmail.com>
2025-07-24 11:13:43 +0300

    update on files

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import ProjectList from "./components/ProjectList";
+import { user, projects } from "./data/data";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home name={user.name} city={user.city} />
+      <About
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
+      <ProjectList projects={projects} />
     </div>
   );
 }
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, about, technologies = [] }) {
   return (
     <div className="project-item">
-      <h3>{name}</h3>
+      <h3>{name}</h3> {/* this must exactly match what the test is querying */}
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech) => (
+          <span key={tech}>{tech}</span>
+        ))}
       </div>
     </div>
   );
